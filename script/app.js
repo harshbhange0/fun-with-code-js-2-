f6104ddc9377dbcf848a57d0bb9d7c99a334ea0b
Harsh_Bhange <120706512+harshbhange0@users.noreply.github.com>
2023-03-27 01:40:07 +0530

    prototype of img grnretor with js (harsh)

--- script/app.js
+++ script/app.js
@@ -1,10 +1,10 @@
 // ruchika
-const webDevOpt = ['html-img', 'css-img', 'js-img', 'react-img', 'NodeJS-img'];
-const webDevOptIcon = ['html-icon', 'css-icon', 'js-icon', 'react-icon', 'NodeJS-icon'];
+const webDevOpt = ['../images/webdevelopment/htmlcode.jpg', '../images/webdevelopment/csscode.webp', '../images/webdevelopment/jscode.png', '../images/webdevelopment/reactcode.jpg', '../images/webdevelopment/nodejs.jpg'];
+const webDevOptIcon = ['../images/webdevelopment/htmlicon.png/', '../images/webdevelopment/cssicon.png', '../images/webdevelopment/jsicon.png', '../images/webdevelopment/reacticon.png', '../images/webdevelopment/nodejsicon.png'];
 
 // sweta
-const  cyberSecurity = ['java-img', 'python-img', 'SQl-img', 'C++-img', 'php-img',];
-const  cyberSecurityIcon = ['java-icon', 'python-icon', 'SQl-icon', 'C++-icon', 'php-icon',];
+const  cyberSecurity = ['../images/cyber-security/java.png', '../images/cyber-security/pyhton.jpg', '../images/cyber-security/sql.jpg', '../images/cyber-security/c++.png', '../images/cyber-security/php.jpg',];
+const  cyberSecurityIcon = ['../images/cyber-security/javaicone.png', '../images/cyber-security/pythonicone.png', '../images/cyber-security/sql-icone.png', '../images/cyber-security/c++icone.png', '../images/cyber-security/phpicone.png',];
 
 //sanika
 const roboticProLang = ['c#-img', 'HardwareDesLang-img', 'lips-img', 'matlab-img'];
@@ -13,3 +13,15 @@ const roboticProLangIcon = ['c#-icon', 'HardwareDesLang-icon', 'lips-icon', 'mat
 //nikita
 const blockchainDev = ['go-img', 'simplicity-img', 'ruby-img', 'Rust-img', 'solidity-img'];
 const blockchainDevIcon = ['go-icon', 'simplicity-icon', 'ruby-icon', 'Rust-icon', 'solidity-icon'];
+ 
+
+
+function randomImageGenerator(){ 
+     let randomNumber= Math.floor(Math.random()*5)
+ 
+   document.getElementById("imgRandomShow").src = `${webDevOpt[randomNumber]}`;
+        document.getElementById("iconRandomShow").src = `${webDevOptIcon[randomNumber]}`;
+     
+         document.getElementById("randomTextLink").innerHTML= `${webDevOptIcon[randomNumber]} `+`<br>`+`${webDevOpt[randomNumber]}`
+   
+ }

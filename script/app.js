ccce809665f2577b318616151ea7de9877d25c4f
Harsh_Bhange <120706512+harshbhange0@users.noreply.github.com>
2023-03-26 12:30:11 +0530

    app.js added (harsh)

---
+++ script/app.js
@@ -0,0 +1,11 @@
+// ruchika
+const webDevOpt = ['html-img', 'css-img', 'js-img', 'react-img', 'NodeJS-img'];
+
+// sweta
+const  cyberSecurity = ['java-img', 'python-img', 'SQl-img', 'C++-img', 'php-img',];
+
+//sanika
+const roboticProLang = ['c#-img', 'HardwareDesLang-img', 'lips-img', 'matlab-img'];
+
+//nikita
+const blockchainDev = ['go-img', 'simplicity-img', 'ruby-img', 'Rust-img', 'solidity-img'];
